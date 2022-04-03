a8fed523e766d43eeeef6dd3d1115e84ae7cc26c
Gutemberg S. Mendoza <gutembergmendoza@Gutembergs-MacBook-Pro.local>
2022-04-02 22:58:13 -0500

    PDF Generator its working properly

--- PDFGenerator.py
+++ PDFGenerator.py
@@ -1,56 +1,98 @@
+from pandas import DataFrame
 from fpdf import FPDF 
 from data_preprocessing import ExcelReader
 
 class PDFGenerator(FPDF): 
-    relative_positivon_y = 75
-    relative_positivon_x = 18
+    # TODO: Documentar el codigo.
+    # This instance variables store the relative position of template
+    relative_position_y = 75
+    relative_position_x = 18
 
     def header(self):
         self.image('bisel/img/header.jpeg',5,0,200)
 
     def footer(self):
         self.image('bisel/img/footer.jpeg',5,220,200)
-        # Page number
+        
 
 
-    def create_template(self, city, email, telf):
+    def create_template(self, city, email, telf, emb, matr,sample_date,test):
         
         self.set_font('Arial', 'B', 10)
-        #self.add_page()
+       
         self.set_text_color(16,44,84)
 
         # Template: Ciudad, Telefono, Email 
-        self.text(self.relative_positivon_x,self.relative_positivon_y, "Ciudad:  ")
-        self.text(self.relative_positivon_x+18,self.relative_positivon_y, city)
+        self.text(self.relative_position_x+10,self.relative_position_y, "Ciudad ")
+        self.text(self.relative_position_x+23,self.relative_position_y, city)
         
-        self.text(self.relative_positivon_x,self.relative_positivon_y+6, "Email: ")
-        self.text(self.relative_positivon_x+18,self.relative_positivon_y+6, email)
+        self.text(self.relative_position_x+10,self.relative_position_y+6, "Email: ")
+        self.text(self.relative_position_x+23,self.relative_position_y+6, email)
         
-        self.text(self.relative_positivon_x,self.relative_positivon_y+12,"Telefono: ")
-        self.text(self.relative_positivon_x+18,self.relative_positivon_y+12, telf)
+        self.text(self.relative_position_x+10,self.relative_position_y+12,"Telefono: ")
+        self.text(self.relative_position_x+27,self.relative_position_y+12, telf)
 
         # Template: Nombre, Cedula, Edad
 
         # rectangulo
-        self.rect(self.relative_positivon_x+7, self.relative_positivon_y+25, self.relative_positivon_y+82,25)
-        self.rect(self.relative_positivon_x+10, self.relative_positivon_y+48, 150,0)
+        self.set_text_color(0,0,0)
+        self.rect(self.relative_position_x+7, self.relative_position_y+25, self.relative_position_y+82,40)
+        self.rect(self.relative_position_x+10, self.relative_position_y+63.5, 153,0)
     
-        self.text(self.relative_positivon_x+10, self.relative_positivon_y+30, "Nombre: ")
-        self.text(self.relative_positivon_x+120, self.relative_positivon_y+30, "Cedula: ")
-        self.text(self.relative_positivon_x+120, self.relative_positivon_y+36, "Edad: ")
-    
-    def populate_pdf(self):
+        self.text(self.relative_position_x+10, self.relative_position_y+30, "EMBARACIÓN: ")
+        self.text(self.relative_position_x+38, self.relative_position_y+30, emb)
+
+
+        self.text(self.relative_position_x+10, self.relative_position_y+36, "NOMBRE: ")
+        self.text(self.relative_position_x+10, self.relative_position_y+42, "FECHA DE TOMA DE MUESTRA: ")
+        self.text(self.relative_position_x+66, self.relative_position_y+42, sample_date)
+
+
+        self.text(self.relative_position_x+10, self.relative_position_y+56, "MÉDICO: Dr. Elvis E. Mendoza Mestanza ")
+        
+        self.text(self.relative_position_x+10, self.relative_position_y+61, "ANÁLISIS: ")
+        self.text(self.relative_position_x+30, self.relative_position_y+61, test)
+
+
+
+        self.text(self.relative_position_x+95, self.relative_position_y+30, "MATRÍCULA: ")
+        self.text(self.relative_position_x+119, self.relative_position_y+30, matr)
+
+
+        self.text(self.relative_position_x+95, self.relative_position_y+36, "CÉDULA: ")
+        self.text(self.relative_position_x+95, self.relative_position_y+42, "FECHA DE NACIMIENTO: ")
+        self.text(self.relative_position_x+95, self.relative_position_y+61, "REGISTRO SANITARIO: 8385-DME-0420 ")
+
+        self.text(self.relative_position_x+20, self.relative_position_y+80, "DETERMINACIÓN DE ANTICUERPOS PARA SARS-COV-2 (COVID-19) ")
+        self.text(self.relative_position_x+20, self.relative_position_y+84.5, "TÉCNICA: ")
+        self.set_font('Arial','',10)
+        self.text(self.relative_position_x+40, self.relative_position_y+84.5, "Determinación de Anticuerpos IgG - IgM inmunocromatográfica.")
+
+        self.set_font('Arial','B',13)
+        self.text(self.relative_position_x+70, self.relative_position_y+108, "RESULTADOS")
+
+        self.set_font('Arial','B',10)
+        self.text(self.relative_position_x+20, self.relative_position_y+120, "ANTICUERPOS IgG")
+        self.text(self.relative_position_x+20, self.relative_position_y+126, "ANTICUERPOS IgM")
+        
+
+
+
+
+
+    def populate_pdf(self, data:DataFrame,city, email, telf, emb, matr,sample_date,test):
         df = ExcelReader('data.xlsx').get_df()
         
-        for name, last_name, identification , date_birth in zip(df['Nombre'], df['Apellido'], df['Cedula'], df['Nacimiento']):
+        for name, last_name, identification , date_birth, status in zip(df['Nombre'], df['Apellido'], df['Cedula'], df['Nacimiento'], df['Estado']):
         # data tokenize
             full_name = name + " " + last_name
             ID = str(identification)
             birth = date_birth.strftime("%m/%d/%Y") 
+            st = str(status)
             
             # populate pdf 
             self.add_page()
-            self.create_template('Manta', 'stev@email.com', '0987868186')
+            self.create_template(city, email, telf, emb, matr,sample_date,test)
            
             self.set_text_color(0,0,0)
             self.set_font('Arial','', 10)
@@ -58,9 +100,10 @@ class PDFGenerator(FPDF):
 
            
             #self.create_template('Manta', 'stev@email.com', '0987868186')
-            self.text(self.relative_positivon_x+27, self.relative_positivon_y+30, full_name)
-            self.text(self.relative_positivon_x+140, self.relative_positivon_y+30, ID)
-            self.text(self.relative_positivon_x+140, self.relative_positivon_y+36, birth)
-
+            self.text(self.relative_position_x+28, self.relative_position_y+36, full_name)
+            self.text(self.relative_position_x+114, self.relative_position_y+36, ID)
+            self.text(self.relative_position_x+140, self.relative_position_y+42, birth)
+            self.text(self.relative_position_x+120, self.relative_position_y+120, st)
+            self.text(self.relative_position_x+120, self.relative_position_y+126, st)
 
         
--- main.py
+++ main.py
@@ -3,35 +3,12 @@
 from data_preprocessing import ExcelReader
 from PDFGenerator import PDFGenerator
 def main():
-    #xlsx to dataframe 
-#     df = ExcelReader('data.xlsx').get_df()
-#    # pdf generation : here its a blank page
-#     pdf = FPDF(orientation = 'P', unit = 'mm', format = 'A4')
-#     pdf.set_font('Arial','', 10)
 
-    # iterate through dataset, and put every row in a different page. 
 
-    # for name, last_name, identification , date_birth in zip(df['Nombre'], df['Apellido'], df['Cedula'], df['Nacimiento']):
-    #     # data tokenize
-    #     full_name = name + " " + last_name
-    #     ID = str(identification)
-    #     birth = date_birth.strftime("%m/%d/%Y") 
-        
-    #     # populate pdf 
-    #     pdf.add_page()
-    #     pdf.text(10,100, full_name)
-    #     pdf.text(100,100, birth)
-    #     pdf.text(160,100, ID)
-
-        
-
-    # pdf output.
-    # pdf.output('foo.pdf', 'F') 
-
-    foo_pdf = PDFGenerator()
-    #foo_pdf.create_template('Manta', "stev@gmail.coom", "09878186")
-    foo_pdf.populate_pdf()
-    foo_pdf.output('foo_2.pdf', 'F')
+    df = ExcelReader('data.xlsx')
+    pdf = PDFGenerator()
+    pdf.populate_pdf(df,'Manta', 'info@biselmed.com', '0987868186', 'SOFIA R', 'P-04-00616', '05/06/2021', 'Prueba Rápida Covid-19')
+    pdf.output('Certificates.pdf', 'F')
 
         
 
