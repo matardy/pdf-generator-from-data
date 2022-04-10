d7a2f02133950ef966514e42d0b9bcb94a01a85a
Gutemberg S. Mendoza <gutembergmendoza@Gutembergs-MacBook-Pro.local>
2022-04-09 21:38:29 -0500

    Update: refactoring function populate_pdf

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def main():
 
     df = ExcelReader('data.xlsx')
     pdf = PDFGenerator()
-    pdf.populate_pdf(df,'Manta', 'info@biselmed.com', '0987868186', 'SOFIA R', 'P-04-00616', '05/06/2021', 'Prueba Rápida Covid-19')
+    #pdf.create_template_embarcacion('Manta', 'info@biselmed.com', '0987868186', 'SOFIA R', 'P-04-00616', '05/06/2021', 'Prueba Rápida Covid-19')
+    pdf.create_template_empresa('Manta', 'info@biselmed.com', '099999999', 'GRUAS VALERIANO', '05/06/2022', 'Prueba PCR')
     pdf.output('Certificates.pdf', 'F')
 
         
